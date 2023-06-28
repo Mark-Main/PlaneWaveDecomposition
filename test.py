bd6daa2e7c016a86a3639038ba48c567f44e6497
Mark Main <mark.main@glasgow.ac.uk>
2023-06-28 16:45:48 +0100

    Sliders now in figure, but not working

--- test.py
+++ test.py
@@ -1,9 +1,6 @@
 import numpy as np
-import scipy.special as sp
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-import ipywidgets as widgets
-from IPython.display import display
+from matplotlib.widgets import Slider
 
 def laguerre_gaussian(x, y, p, l, w0, z, k):
     """
@@ -24,8 +21,8 @@ def laguerre_gaussian(x, y, p, l, w0, z, k):
     rho = np.sqrt(x**2 + y**2)
     phi = np.arctan2(y, x)
 
-    radial = np.sqrt(2 * sp.factorial(p) / (np.pi * sp.factorial(p + np.abs(l))))
-    laguerre = sp.assoc_laguerre(np.abs(l), p, 2 * rho**2 / w0**2)
+    radial = np.sqrt(2 * np.math.factorial(p) / (np.pi * np.math.factorial(p + np.abs(l))))
+    laguerre = np.polynomial.Legendre.basis(l)(2 * rho**2 / w0**2)
     gauss = np.exp(-rho**2 / w0**2)
 
     phase = np.exp(1j * (l * phi + k * rho**2 / (2 * z)))
@@ -41,48 +38,44 @@ X, Y = np.meshgrid(x, y)
 p = 1  # Radial mode
 l = 2  # Azimuthal mode
 w0 = 2  # Waist parameter
-z = 10  # Propagation distance
+z = 100  # Propagation distance
 k = 2 * np.pi / 0.5  # Wave number
 
-# Function to update the plot when the slider value changes
-def update_plot(l):
-    # Generate LG wave with the updated l value
-    lg_wave = laguerre_gaussian(X, Y, p, l, w0, z, k)
-
-    # Clear previous plot
-    ax.clear()
-
-    # Plot the intensity profile in 3D
-    intensity = np.abs(lg_wave)**2
-    ax.plot_surface(X, Y, intensity, cmap='hot')
-    ax.set_xlabel('X')
-    ax.set_ylabel('Y')
-    ax.set_zlabel('Intensity')
-    ax.set_title(f'Laguerre-Gaussian (p={p}, l={l}) at z={z}')
-
-    plt.show()
-
-# Create the figure and axis for the plot
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
+# Create subplot
+fig, ax = plt.subplots(subplot_kw={'projection': '3d'})
 
 # Generate initial LG wave
 lg_wave = laguerre_gaussian(X, Y, p, l, w0, z, k)
 
 # Plot the intensity profile in 3D
 intensity = np.abs(lg_wave)**2
-ax.plot_surface(X, Y, intensity, cmap='hot')
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-ax.set_zlabel('Intensity')
-ax.set_title(f'Laguerre-Gaussian (p={p}, l={l}) at z={z}')
+surf = ax.plot_surface(X, Y, intensity, cmap='hot')
 
-# Create the slider
-l_slider = widgets.IntSlider(value=l, min=-5, max=5, description='l')
+# Create axes for p and l sliders
+ax_p = plt.axes([0.25, 0.15, 0.65, 0.03])
+ax_l = plt.axes([0.25, 0.1, 0.65, 0.03])
 
-# Link the slider to the update_plot function
-l_slider.observe(lambda change: update_plot(change['new']), names='value')
+# Create p and l sliders with initial values
+slider_p = Slider(ax_p, 'p', 0, 5, valinit=p, valstep=1)
+slider_l = Slider(ax_l, 'l', -5, 5, valinit=l, valstep=1)
 
-# Display the slider and initial plot
-display(l_slider)
+# Function to update the plot when the sliders' values change
+def update_plot(val):
+    p_val = int(slider_p.val)
+    l_val = int(slider_l.val)
+
+    # Generate LG wave with the updated p and l values
+    lg_wave = laguerre_gaussian(X, Y, p_val, l_val, w0, z, k)
+
+    # Update the intensity profile
+    intensity = np.abs(lg_wave)**2
+    surf.set_array(intensity.ravel())
+    fig.canvas.draw_idle()
+
+# Link the sliders to the update_plot function
+slider_p.on_changed(update_plot)
+slider_l.on_changed(update_plot)
+
+# Display the sliders and initial plot
 plt.show()
+
