0f5926fa01440adb9aced7b83744bf1f022077f3
Mark Main <mark.main@glasgow.ac.uk>
2023-06-28 16:19:21 +0100

    Moving code to PyCharm

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
 import numpy as np
 import scipy.special as sp
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import ipywidgets as widgets
+from IPython.display import display
 
 def laguerre_gaussian(x, y, p, l, w0, z, k):
     """
@@ -41,14 +44,45 @@ w0 = 2  # Waist parameter
 z = 10  # Propagation distance
 k = 2 * np.pi / 0.5  # Wave number
 
-# Generate LG wave
+# Function to update the plot when the slider value changes
+def update_plot(l):
+    # Generate LG wave with the updated l value
+    lg_wave = laguerre_gaussian(X, Y, p, l, w0, z, k)
+
+    # Clear previous plot
+    ax.clear()
+
+    # Plot the intensity profile in 3D
+    intensity = np.abs(lg_wave)**2
+    ax.plot_surface(X, Y, intensity, cmap='hot')
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Intensity')
+    ax.set_title(f'Laguerre-Gaussian (p={p}, l={l}) at z={z}')
+
+    plt.show()
+
+# Create the figure and axis for the plot
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+# Generate initial LG wave
 lg_wave = laguerre_gaussian(X, Y, p, l, w0, z, k)
 
-# Plot the intensity profile
+# Plot the intensity profile in 3D
 intensity = np.abs(lg_wave)**2
-plt.imshow(intensity, extent=(-10, 10, -10, 10), cmap='hot')
-plt.colorbar(label='Intensity')
-plt.xlabel('X')
-plt.ylabel('Y')
-plt.title(f'Laguerre-Gaussian (p={p}, l={l}) at z={z}')
+ax.plot_surface(X, Y, intensity, cmap='hot')
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Intensity')
+ax.set_title(f'Laguerre-Gaussian (p={p}, l={l}) at z={z}')
+
+# Create the slider
+l_slider = widgets.IntSlider(value=l, min=-5, max=5, description='l')
+
+# Link the slider to the update_plot function
+l_slider.observe(lambda change: update_plot(change['new']), names='value')
+
+# Display the slider and initial plot
+display(l_slider)
 plt.show()
