f2b098e5e55be2e1f046555e89d6893c04aa3ed6
Mark-Main <mark.main@glasgow.ac.uk>
2023-07-03 11:04:41 +0100

    Added some fixes, slider initial value was differnt from reality, creating a strange error in the phase, aslo added 0 mimimum to the colorbar. Still need to fix lack of beam warping as distance increases

--- dist2D.py
+++ dist2D.py
@@ -5,20 +5,19 @@ from scipy.special import genlaguerre
 import generateLaguerre2DnoZ
 import distanceTerm
 
-
 # Parameters
-x = np.linspace(-10, 10, 500)
-y = np.linspace(-10, 10, 500)
+x = np.linspace(-10, 10, 512)
+y = np.linspace(-10, 10, 512)
 X, Y = np.meshgrid(x, y)
 p_init = 0  # Initial radial mode
 l_init = 0  # Initial azimuthal mode
 w0 = 2  # Waist parameter
-z_init = 0  # Propagation distance
+z_init = 1  # Propagation distance
 k = 2 * np.pi / 0.5  # Wave number
 
-res = 500
+res = 512
 s = 1
-λ = 1550/1000000000
+λ = 400 / 1000000000
 
 plt.close('all')
 
@@ -27,20 +26,18 @@ fig, ax = plt.subplots(nrows=2, ncols=1, figsize=(20, 20))
 plt.subplots_adjust(left=0.25, bottom=0.3)
 
 # Create and customize the first subplot using ax[0]
-
 ax[0].set_xlabel('X1')
 ax[0].set_ylabel('Y1')
 ax[0].set_title('Intensity')
 
 # Create and customize the second subplot using ax[1]
-
 ax[1].set_xlabel('X2')
 ax[1].set_ylabel('Y2')
 ax[1].set_title('Phase')
 
 # Generate the initial Laguerre-Gaussian beam
-wave = generateLaguerre2DnoZ.laguerre_gaussian(X, Y, p_init, l_init, w0 ,k)
-distance = distanceTerm.disStep(z_init, res, s , λ)
+wave = generateLaguerre2DnoZ.laguerre_gaussian(X, Y, p_init, l_init, w0, k)
+distance = distanceTerm.disStep(z_init, res, s, λ)
 
 # Multiply wave and distance arrays
 product = np.multiply(wave, distance)
@@ -51,21 +48,24 @@ fft_result = np.fft.fft2(product)
 # Perform inverse Fourier transform
 ifft_result = np.fft.ifft2(fft_result)
 
-
-
 # Normalize the intensity
-intensity = np.abs(ifft_result)**2
-#intensity /= np.max(intensity)
+intensity = np.abs(ifft_result) ** 2
 
-#Identify the phase
-
+# Identify the phase
 phase = np.angle(ifft_result)
 
 # Plot the initial Laguerre-Gaussian beam
 cax = ax[0].imshow(intensity, extent=[x[0], x[-1], y[0], y[-1]], cmap='inferno', origin='lower')
 cax2 = ax[1].imshow(phase, extent=[x[0], x[-1], y[0], y[-1]], cmap='inferno', origin='lower')
-plt.colorbar(cax)
-plt.colorbar(cax2)
+
+# Create the colorbars
+colorbar1 = plt.colorbar(cax, ax=ax[0])
+colorbar2 = plt.colorbar(cax2, ax=ax[1])
+
+# Set the minimum value to 0 for both colorbars
+colorbar1.mappable.set_clim(vmin=0)
+colorbar2.mappable.set_clim(vmin=0)
+
 
 # Create the slider axes
 ax_l = plt.axes([0.1, 0.3, 0.05, 0.6])
@@ -75,7 +75,8 @@ ax_z = plt.axes([0, 0.3, 0.05, 0.6])
 # Create the sliders
 slider_l = Slider(ax=ax_l, label='l', valmin=0, valmax=10, valstep=1, valinit=l_init, orientation='vertical')
 slider_p = Slider(ax=ax_p, label='p', valmin=0, valmax=10, valstep=1, valinit=p_init, orientation='vertical')
-slider_z = Slider(ax=ax_z, label='z', valmin=0, valmax=100, valstep=5, valinit=z_init, orientation='vertical')
+slider_z = Slider(ax=ax_z, label='z', valmin=1, valmax=100, valstep=1, valinit=z_init, orientation='vertical')
+
 
 # Update function for the sliders
 def update(val):
@@ -86,7 +87,7 @@ def update(val):
 
     # Generate the Laguerre-Gaussian beam
     wave = generateLaguerre2DnoZ.laguerre_gaussian(X, Y, p, l, w0, k)
-    distance = distanceTerm.disStep(z, res, s , λ)
+    distance = distanceTerm.disStep(z, res, s, λ)
 
     # Multiply wave and distance arrays
     product = np.multiply(wave, distance)
@@ -98,17 +99,16 @@ def update(val):
     ifft_result = np.fft.ifft2(fft_result)
 
     # Normalize the intensity
-    intensity = np.abs(ifft_result)**2
-    #intensity /= np.max(intensity)
+    intensity = np.abs(ifft_result) ** 2
 
-    #Identify the phase
-
+    # Identify the phase
     phase = np.angle(ifft_result)
 
     # Update the plot
     cax.set_data(intensity)
     cax2.set_data(phase)
     fig.canvas.draw_idle()
+
 
 # Register the update function with the sliders
 slider_l.on_changed(update)
--- distanceTerm.py
+++ distanceTerm.py
@@ -10,21 +10,23 @@ def disStep(d, res, s, λ):
             # Term 1: First component of the distance factor
             # It represents the square of (2 * π / λ)
             term1 = (6.28318 / λ) ** 2
+           # print(term1)
 
             # Term 2: Second component of the distance factor
             # It calculates the horizontal distance from the center to the current point
             # by wrapping the indices around within the range of res
             # The distance is scaled by s and squared
             term2 = (2 * np.pi * ((x + res / 2) % res - res / 2) / s) ** 2
-
+           # print(term2)
             # Term 3: Third component of the distance factor
             # It calculates the vertical distance from the center to the current point
             # by wrapping the indices around within the range of res
             # The distance is scaled by s and squared
             term3 = (2 * np.pi * ((y + res / 2) % res - res / 2) / s) ** 2
-
+            #print(term3)
             # Calculate the distance factor by subtracting term2 and term3 from term1 and taking the square root
             distance_factor = np.sqrt(term1 - term2 - term3)
+            #print(distance_factor)
 
             # Compute the complex value by multiplying with d and exponentiating with (0 + 1j)
             result[x, y] = d * np.exp(distance_factor * (0 + 1j))
