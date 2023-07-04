Fixed a lot of the bugs, now phase and intensity are scaling with distance, values were modified to align with martins, beam is not moving in intensity pattern

distanceTerm.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 def disStep(d, res, s, λ):
     # Create an empty complex array of size (res, res)
@@ -13,22 +14,18 @@ def disStep(d, res, s, λ):
            # print(term1)
 
             # Term 2: Second component of the distance factor
-            # It calculates the horizontal distance from the center to the current point
-            # by wrapping the indices around within the range of res
-            # The distance is scaled by s and squared
+
             term2 = (2 * np.pi * ((x + res / 2) % res - res / 2) / s) ** 2
            # print(term2)
             # Term 3: Third component of the distance factor
-            # It calculates the vertical distance from the center to the current point
-            # by wrapping the indices around within the range of res
-            # The distance is scaled by s and squared
+            
             term3 = (2 * np.pi * ((y + res / 2) % res - res / 2) / s) ** 2
             #print(term3)
             # Calculate the distance factor by subtracting term2 and term3 from term1 and taking the square root
             distance_factor = np.sqrt(term1 - (term2 + term3))
             #print(distance_factor)
-
+            
             # Compute the complex value by multiplying with d and exponentiating with (0 + 1j)
             result[x, y] = np.exp(d* distance_factor * (0 + 1j))
-
+    
     return result

generateLaguerre2DnoZ.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider
 from scipy.special import genlaguerre
 
-def laguerre_gaussian(x, y, p, l, w0, k):
+def laguerre_gaussian(x, y, p, l, w0):
     """
     Generate a Laguerre-Gaussian (LG) beam.
 
@@ -13,6 +13,7 @@ def laguerre_gaussian(x, y, p, l, w0, k):
         p (int): Radial mode.
         l (int): Azimuthal mode.
         w0 (float): Waist parameter.
+        z (float): Propagation distance.
         k (float): Wave number.
 
     Returns:
@@ -26,14 +27,12 @@ def laguerre_gaussian(x, y, p, l, w0, k):
     azimuthal_part = np.exp(1j * l * theta)
 
     # Calculate the Gaussian factor
-#
+   # gaussian_factor = np.exp(-1j * k * r**2 / (2 * (z + 1j * k * w0**2)))
 
     # Calculate the Laguerre polynomial
     laguerre_poly = genlaguerre(p, l)(2 * r**2 / w0**2)
 
     # Calculate the Laguerre-Gaussian beam
-    laguerre_gaussian_beam = radial_part * azimuthal_part  * laguerre_poly
-    #print(laguerre_gaussian_beam)
+    laguerre_gaussian_beam = radial_part * azimuthal_part * laguerre_poly
 
     return laguerre_gaussian_beam
-

tilt_func.py
@@ -6,6 +6,5 @@ def tilttip(res, phaseshifttip, phaseshifttilt):
     x = np.arange(1, res + 1, 1)
     y = np.arange(1, res + 1, 1)
     X, Y = np.meshgrid(x, y)
-    Z = X * phaseshifttip / res + Y * phaseshifttilt / res
-    print(X)
+    Z = (X * phaseshifttip / res) + (Y * phaseshifttilt / res)
     return X, Y, Z