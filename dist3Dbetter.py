5e776bb2d46bfa3d4637f72dd850e1ec1ef31d20
Mark-Main <mark.main@glasgow.ac.uk>
2023-07-08 11:45:07 +0100

    Made a working version with the exception of wrapping in the fourier plane. Which si annoying. Need a supergaussina to fix

--- dist3Dbetter.py
+++ dist3Dbetter.py
@@ -2,20 +2,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.widgets import TextBox
 from mpl_toolkits.mplot3d import Axes3D
-from scipy.special import genlaguerre
+from scipy.special import genlaguerre, eval_hermite
 import generateLaguerre2DnoZ
 import distanceTerm
 import tilt_func
+import csv
 
 # Parameters
-res =64
-x = np.linspace(-0.025, 0.025, res)
-y = np.linspace(-0.025, 0.025, res)
+res = 128
+x = np.linspace(-0.25, 0.25, res)
+y = np.linspace(-0.25, 0.25, res)
 X, Y = np.meshgrid(x, y)
-p_init = 0  # Initial radial mode
-l_init = 3  # Initial azimuthal mode
-w0 = 0.002  # Waist parameter
-z_init = [0,10,100]  # Propagation distances for slices
+
+p_init = 1  # Initial radial mode
+l_init = 0  # Initial azimuthal mode
+w0 = 0.02  # Waist parameter
+z_init = [0, 3, 10, 50]  # Propagation distances for slices
 
 s = 0.025
 λ = 600 / 1000000000
@@ -28,7 +30,7 @@ fig = plt.figure(figsize=(20, 25))
 ax = fig.add_subplot(121, projection='3d')  # Left subplot
 ax2 = fig.add_subplot(122, projection='3d')  # Right subplot
 
-# Create text boxes for phase shift 
+# Create text boxes for phase shift
 tip_text_box = TextBox(plt.axes([0.85, 0.9, 0.1, 0.05]), 'Phase Shift Tip', initial=str(phaseshifttip_init))
 tilt_text_box = TextBox(plt.axes([0.85, 0.85, 0.1, 0.05]), 'Phase Shift Tilt', initial=str(phaseshifttilt_init))
 
@@ -59,6 +61,29 @@ def update(val):
         distance = distanceTerm.disStep(z_init[i], res, s, λ)
         L, M, N = tilt_func.tilttip(res, phaseshifttip, phaseshifttilt)
 
+        # Create a supergaussian meshgrid
+        xx = np.linspace(-1, 1, res * 10)
+        yy = np.linspace(-1, 1, res * 10)
+        XX, YY = np.meshgrid(xx, yy)
+
+        # Create the supergaussian function
+        sigma = 0.1
+        exponent = -((XX / sigma) ** 4 + (YY / sigma) ** 4)
+        supergaussian = np.exp(exponent)
+
+        # Calculate the dimensions of the wave array
+        wave_size = wave.shape[0]
+
+        # Calculate the center position for adding wave to bigplot
+        center_x = supergaussian.shape[0] // 2
+        center_y = supergaussian.shape[1] // 2
+
+        # Calculate the indices for adding the wave to the center of bigplot
+        x_start = center_x - wave_size // 2
+        x_end = x_start + wave_size
+        y_start = center_y - wave_size // 2
+        y_end = y_start + wave_size
+
         # Multiply wave and distance arrays
         product = np.multiply(wave, np.exp(1j * N))
 
@@ -68,9 +93,19 @@ def update(val):
         # Perform inverse Fourier transform
         ifft_result = np.fft.ifft2(fft_result * distance)
 
+        # Add the wave to the center of the supergaussian
+        supergaussian[x_start:x_end, y_start:y_end] += ifft_result
+
         # Normalize the intensity
         intensity = np.abs(ifft_result) ** 2
         intensity /= np.max(intensity)
+
+        filename = 'array_data.csv'
+        with open(filename, 'w', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerows(intensity)
+
+        print(f"Array data saved as {filename}")
 
         phase = np.angle(ifft_result)
 
@@ -78,21 +113,18 @@ def update(val):
         cmap = plt.cm.inferno
 
         # Plot the 2D slice in the 3D plot with transparent areas
-        ax.plot_surface(X*10, Y*10, z_init[i] * np.ones_like(X), facecolors=cmap(intensity),
+        ax.plot_surface(X, Y, z_init[i] * np.ones_like(X), facecolors=cmap(intensity),
                         rstride=1, cstride=1, shade=False)
 
         ax2.plot_surface(X, Y, z_init[i] * np.ones_like(X), facecolors=cmap(phase),
                          rstride=1, cstride=1, shade=False)
 
     # Set limits and labels for the 3D plots
-    ax.set_xlim(-0.1, 0.1)
-    ax.set_ylim(-0.1, 0.1)
     ax.set_zlim(0, max(z_init))
     ax.set_xlabel('X')
     ax.set_ylabel('Y')
     ax.set_zlabel('Distance')
     ax.set_title('2D Intensity Plots in 3D')
-
 
     ax2.set_zlim(0, max(z_init))
     ax2.set_xlabel('X')
